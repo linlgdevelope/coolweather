2152f8a5a86182075d6a88a6da3da7706b35f4d0
Mike <linlgdevelope@sina.com>
2017-12-29 17:39:57 +0800

    新增切换城市和手动更新天气的功能

---
+++ app/src/main/java/com/coolweather/android/gson/AQI.java
@@ -0,0 +1,18 @@
+package com.coolweather.android.gson;
+
+/**
+ * Created by Michael on 2017/12/28.
+ */
+
+public class AQI {
+    public AQICity city;
+
+    public class AQICity {
+
+        public String aqi;
+
+        public String pm25;
+
+    }
+
+}
---
+++ app/src/main/java/com/coolweather/android/gson/Now.java
@@ -0,0 +1,23 @@
+package com.coolweather.android.gson;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by Michael on 2017/12/28.
+ */
+
+public class Now {
+
+    @SerializedName("tmp")
+    public String temperature;
+
+    @SerializedName("cond")
+    public More more;
+
+    public class More {
+
+        @SerializedName("txt")
+        public String info;
+
+    }
+}
